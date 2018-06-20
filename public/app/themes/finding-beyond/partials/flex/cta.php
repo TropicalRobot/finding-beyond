337d3fc35405d61381a22da614a4962dc4a24808
Ryan Griffiths <ryan@3ev.com>
2018-06-20 19:43:16 +0000

    full width cta overlay/shadow

--- public/app/themes/finding-beyond/partials/flex/cta.php
+++ public/app/themes/finding-beyond/partials/flex/cta.php
@@ -1,14 +1,14 @@
 <?php $link = getACFLink($item);?>
 
-<section class="full-width-section cta cta--full-width cta--bg-overlay <?php echo $link['url'] ? 'cta--full-width-linked' : ''; ?>">
-    <div class="cta__bg-img" style="background-image:url(<?php echo $item->field('image')->val(); ?>)"></div>
+<section class="full-width-section cta cta--full-width <?php echo $link['url'] ? 'cta--full-width-linked' : ''; ?>">
+    <div class="cta__bg-img" style="background-image:url(<?php echo $item->field('image')->sizeUrl('hero'); ?>)"></div>
 
     <div class="cta__header">
         <?php if ($item->field('heading')->val() != ''): ?>
             <h1 class="cta__heading"><?php echo $item->field('heading')->val(); ?></h1>
         <?php endif; ?>
 
-        <p><?php echo $item->field('text')->val(); ?></p>
+        <?php echo $item->field('text')->val(); ?>
     </div>
 
     <?php if ($link['url'] && $item->field('link_text')->val()): ?>
